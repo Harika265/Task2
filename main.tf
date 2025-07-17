86f5a214cd8bea3088786db823e781b92599c1f4
Harika265 <harikavemareddy020@gmail.com>
2025-07-17 16:53:47 +0000

    Add GitHub Actions workflow for Azure Static Web App

--- main.tf
+++ main.tf
@@ -6,4 +6,15 @@ resource "azurerm_resource_group" "rg" {
   name     = "example-rg"
   location = "East US"
 }
-
+resource "azurerm_storage_account" "storage" {
+  name                     = "vinstorageacctdemo"        # must be globally unique, 3–24 lowercase letters/numbers
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+resource "azurerm_storage_container" "container" {
+  name                  = "vin-container"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"   # or "blob" for public read
+}
